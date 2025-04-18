a3b341fecc799dc9076373108b055d70e84b0360
MagoCitu357 <137245741+MagoCitu357@users.noreply.github.com>
2025-04-18 14:30:55 -0300

    prototipo v1

---
+++ precios.js
@@ -0,0 +1,6 @@
+const precios = {
+    hamburguesa: 6.99,
+    pizza: 8.5,
+    ensalada: 5.75,
+    tacos: 7.25
+  };
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("precio-hamburguesa").textContent = `$${precios.hamburguesa.toFixed(2)}`;
+document.getElementById("precio-pizza").textContent = `$${precios.pizza.toFixed(2)}`;
+document.getElementById("precio-ensalada").textContent = `$${precios.ensalada.toFixed(2)}`;
+document.getElementById("precio-tacos").textContent = `$${precios.tacos.toFixed(2)}`;
